bb624e1de36e021d8368055911d73421d307f4a1
new-cde <abinaya.muthukrishnan.3006@gmail.com>
2026-03-16 14:26:43 +0530

    UC1: Application Entry & Welcome Message

--- src/Book_My_Stay.java
+++ src/Book_My_Stay.java
@@ -1,2 +1,10 @@
 public class Book_My_Stay{
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // End message
+        System.out.println("System initialized successfully.");
+    }
 }
